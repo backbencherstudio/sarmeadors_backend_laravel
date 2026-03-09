b9b5085fb48e5eabb2f74c2ead232f31d9f66679
Niaz Ahmed Nayeem <niazahmed.net@gmail.com>
2026-03-09 13:21:40 +0600

    Update agency event type model

--- app/Models/AgencyEventType.php
+++ app/Models/AgencyEventType.php
@@ -3,8 +3,20 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class AgencyEventType extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'agency_id',
+        'name',
+        'status',
+    ];
+
+    public function agency()
+    {
+        return $this->belongsTo(Agency::class);
+    }
 }
