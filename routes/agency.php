c1d4d8ccbd2655ff9b76ccac64b536bb4f7bc96f
Mahmudul Elahi <mahmudul.elahi@gmail.com>
2026-06-10 15:55:20 +0600

    rename: routes

---
+++ routes/agency.php
@@ -0,0 +1,229 @@
+<?php
+
+use App\Http\Controllers\Agency\AgencyChecklistController;
+use App\Http\Controllers\Agency\AgencyController;
+use App\Http\Controllers\Agency\AgencyEventController;
+use App\Http\Controllers\Agency\AgencyEventTypeController;
+use App\Http\Controllers\Agency\AgencyLocationController;
+use App\Http\Controllers\Agency\AgencyNoteController;
+use App\Http\Controllers\Agency\AgencySettingsController;
+use App\Http\Controllers\Agency\AgencyTagController;
+use App\Http\Controllers\Agency\AgencyTypeController;
+use App\Http\Controllers\Agency\ClientController;
+use App\Http\Controllers\Agency\DocumentTemplateController;
+use App\Http\Controllers\Agency\FormController;
+use App\Http\Controllers\Agency\FormFieldController;
+use App\Http\Controllers\Agency\JobMessageController as AgencyJobMessageController;
+use App\Http\Controllers\Agency\LongTermJobApplicationController as AgencyLongTermJobApplicationController;
+use App\Http\Controllers\Agency\LongTermJobAttendanceController as AgencyLongTermJobAttendanceController;
+use App\Http\Controllers\Agency\LongTermJobController as AgencyLongTermJobController;
+use App\Http\Controllers\Agency\LongTermJobRefundController as AgencyLongTermJobRefundController;
+use App\Http\Controllers\Agency\MessageTemplateController;
+use App\Http\Controllers\Agency\ServiceController;
+use App\Http\Controllers\Agency\SettingsController;
+use App\Http\Controllers\Agency\ShortTermJobApplicationController as AgencyShortTermJobApplicationController;
+use App\Http\Controllers\Agency\ShortTermJobAttendanceController as AgencyShortTermJobAttendanceController;
+use App\Http\Controllers\Agency\ShortTermJobController as AgencyShortTermJobController;
+use App\Http\Controllers\Agency\ShortTermJobRefundController as AgencyShortTermJobRefundController;
+use App\Http\Controllers\Agency\StatusController;
+use App\Http\Controllers\Agency\StatusTemplateController;
+use App\Http\Controllers\Agency\SubLocationController;
+use App\Http\Controllers\Messages\ShortTermJobMessageController;
+use Illuminate\Support\Facades\Route;
+
+Route::middleware(['subdomain', 'auth:api', 'role:agency_admin'])->prefix('agency')->group(function () {
+    Route::get('agency_notes/{user}', [AgencyNoteController::class, 'index']);
+    Route::post('agency_notes/{user}', [AgencyNoteController::class, 'store']);
+    Route::get('agency_notes/show/{agency_note}', [AgencyNoteController::class, 'show']);
+    Route::put('agency_notes/{agency_note}', [AgencyNoteController::class, 'update']);
+    Route::put('agency_notes/pin_note/{agency_note}', [AgencyNoteController::class, 'pin_note']);
+    Route::put('agency_notes/mark_read', [AgencyNoteController::class, 'mark_read']);
+    Route::delete('agency_notes', [AgencyNoteController::class, 'destroy']);
+
+    Route::get('message_template', [MessageTemplateController::class, 'index']);
+    Route::post('message_template', [MessageTemplateController::class, 'store']);
+    Route::get('message_template/{messageTemplate}', [MessageTemplateController::class, 'show']);
+    Route::put('message_template/{messageTemplate}', [MessageTemplateController::class, 'update']);
+    Route::delete('message_template', [MessageTemplateController::class, 'destroy']);
+
+    // Stripe credentials
+    Route::get('settings/stripe', [AgencySettingsController::class, 'getStripeStatus']);
+    Route::post('settings/stripe', [AgencySettingsController::class, 'saveStripeKeys']);
+    Route::delete('settings/stripe', [AgencySettingsController::class, 'removeStripeKeys']);
+
+    // Short-term job fee settings
+    Route::get('settings/short-term-fee', [AgencySettingsController::class, 'getShortTermFeeSettings']);
+    Route::put('settings/short-term-fee', [AgencySettingsController::class, 'updateShortTermFeeSettings']);
+
+    // Short-term job approval settings
+    Route::get('settings/short-term-approval', [AgencySettingsController::class, 'getShortTermApprovalSettings']);
+    Route::put('settings/short-term-approval', [AgencySettingsController::class, 'updateShortTermApprovalSettings']);
+
+    // Agency short-term job management
+    Route::get('jobs/short-term', [AgencyShortTermJobController::class, 'index']);
+    Route::get('jobs/short-term/{shortTermJob}', [AgencyShortTermJobController::class, 'show']);
+    Route::put('jobs/short-term/{shortTermJob}/approve', [AgencyShortTermJobController::class, 'approve']);
+    Route::put('jobs/short-term/{shortTermJob}/reject', [AgencyShortTermJobController::class, 'reject']);
+    Route::put('jobs/short-term/{shortTermJob}/cancel', [AgencyShortTermJobController::class, 'cancel']);
+    Route::put('jobs/short-term/{shortTermJob}/complete', [AgencyShortTermJobController::class, 'complete']);
+
+    // Agency short-term applicants
+    Route::get('jobs/short-term/{shortTermJob}/applicants', [AgencyShortTermJobApplicationController::class, 'index']);
+    Route::get('jobs/short-term/{shortTermJob}/applicants/{applicationId}', [AgencyShortTermJobApplicationController::class, 'show']);
+    Route::put('jobs/short-term/{shortTermJob}/applicants/{applicationId}/confirm-hire', [AgencyShortTermJobApplicationController::class, 'confirmHire']);
+
+    // Agency short-term attendance
+    Route::get('jobs/short-term/{shortTermJob}/attendance', [AgencyShortTermJobAttendanceController::class, 'index']);
+
+    // Agency short-term messages
+    Route::get('jobs/short-term/{shortTermJob}/messages', [ShortTermJobMessageController::class, 'index']);
+    Route::post('jobs/short-term/{shortTermJob}/messages', [ShortTermJobMessageController::class, 'store']);
+    Route::get('jobs/short-term/{shortTermJob}/messages/unread-counts', [ShortTermJobMessageController::class, 'unreadCounts']);
+
+    // Agency short-term refund
+    Route::get('jobs/short-term/{shortTermJob}/refund-request', [AgencyShortTermJobRefundController::class, 'show']);
+    Route::put('jobs/short-term/{shortTermJob}/refund-request/approve', [AgencyShortTermJobRefundController::class, 'approve']);
+    Route::put('jobs/short-term/{shortTermJob}/refund-request/reject', [AgencyShortTermJobRefundController::class, 'reject']);
+
+    // Agency long-term job management
+    Route::get('jobs/long-term', [AgencyLongTermJobController::class, 'index']);
+    Route::get('jobs/long-term/{longTermJob}', [AgencyLongTermJobController::class, 'show']);
+    Route::put('jobs/long-term/{longTermJob}/approve', [AgencyLongTermJobController::class, 'approve']);
+    Route::put('jobs/long-term/{longTermJob}/reject', [AgencyLongTermJobController::class, 'reject']);
+    Route::put('jobs/long-term/{longTermJob}/complete', [AgencyLongTermJobController::class, 'complete']);
+    Route::put('jobs/long-term/{longTermJob}/assign-nanny', [AgencyLongTermJobAttendanceController::class, 'assignNanny']);
+
+    // Agency long-term attendance calendar & nanny payments
+    Route::get('jobs/long-term/{longTermJob}/attendance', [AgencyLongTermJobAttendanceController::class, 'calendar']);
+    Route::post('jobs/long-term/{longTermJob}/attendance', [AgencyLongTermJobAttendanceController::class, 'upsert']);
+    Route::get('jobs/long-term/{longTermJob}/nanny-payments', [AgencyLongTermJobAttendanceController::class, 'listNannyPayments']);
+    Route::post('jobs/long-term/{longTermJob}/nanny-payments', [AgencyLongTermJobAttendanceController::class, 'recordNannyPayment']);
+
+    // Agency long-term job messages (both client & candidate threads)
+    Route::get('jobs/long-term/{longTermJob}/messages', [AgencyJobMessageController::class, 'index']);
+    Route::post('jobs/long-term/{longTermJob}/messages', [AgencyJobMessageController::class, 'store']);
+    Route::get('jobs/long-term/{longTermJob}/messages/unread-counts', [AgencyJobMessageController::class, 'unreadCounts']);
+
+    // Agency long-term job applications management
+    Route::get('jobs/long-term/{longTermJob}/applicants', [AgencyLongTermJobApplicationController::class, 'index']);
+    Route::get('jobs/long-term/{longTermJob}/applicants/{application}', [AgencyLongTermJobApplicationController::class, 'show']);
+    Route::put('jobs/long-term/{longTermJob}/applicants/{application}/confirm-hire', [AgencyLongTermJobApplicationController::class, 'confirmHire']);
+
+    // Agency long-term refund request management
+    Route::get('jobs/long-term/{longTermJob}/refund-request', [AgencyLongTermJobRefundController::class, 'show']);
+    Route::put('jobs/long-term/{longTermJob}/refund-request/approve', [AgencyLongTermJobRefundController::class, 'approve']);
+    Route::put('jobs/long-term/{longTermJob}/refund-request/reject', [AgencyLongTermJobRefundController::class, 'reject']);
+
+    // agency sub-location management route for global settings
+    Route::get('sub-locations', [SubLocationController::class, 'index']);
+    Route::post('sub-location-store', [SubLocationController::class, 'store']);
+    Route::get('sub-location/{id}', [SubLocationController::class, 'show']);
+    Route::put('sub-location-update/{id}', [SubLocationController::class, 'update']);
+    Route::delete('sub-location-destroy/{id}', [SubLocationController::class, 'destroy']);
+
+    // agency service management route for global settings
+    Route::get('/services', [ServiceController::class, 'index']);
+    Route::post('/service-store', [ServiceController::class, 'store']);
+    Route::get('/service/{id}', [ServiceController::class, 'show']);
+    Route::put('/service-update/{id}', [ServiceController::class, 'update']);
+    Route::delete('/service-destroy/{id}', [ServiceController::class, 'destroy']);
+    Route::patch('/service-change-status/{id}', [ServiceController::class, 'changeStatus']);
+
+    // agency business details management route for global settings
+    Route::get('/business-details', [SettingsController::class, 'getBusinessDetails']);
+    Route::post('/business-holiday-store', [SettingsController::class, 'storeHoliday']);
+    Route::delete('/business-holiday-delete/{id}', [SettingsController::class, 'destroyHoliday']);
+    Route::post('/business-setting-update', [SettingsController::class, 'updateBusinessSettings']);
+    Route::post('/business-hour-update', [SettingsController::class, 'updateBusinessHours']);
+    Route::patch('/business-hours/{id}/status', [SettingsController::class, 'toggleBusinessHourStatus']);
+
+    Route::get('/agency-info', [AgencyController::class, 'info']);
+    Route::post('info-update/{id}', [AgencyController::class, 'infoUpdate']);
+});
+
+Route::middleware(['subdomain', 'auth:api', 'role:agency_admin|agency_staff'])->prefix('agency')->group(function () {
+
+    // agency type management route
+    Route::get('types', [AgencyTypeController::class, 'index']);
+    Route::get('type/{id}', [AgencyTypeController::class, 'show']);
+    Route::post('type-store', [AgencyTypeController::class, 'store']);
+    Route::put('type-update/{id}', [AgencyTypeController::class, 'update']);
+    Route::put('type-bulk-update', [AgencyTypeController::class, 'bulkUpdate']);
+    Route::delete('type-destroy/{id}', [AgencyTypeController::class, 'destroy']);
+
+    // agency location management route
+    Route::get('locations', [AgencyLocationController::class, 'index']);
+    Route::get('location/{id}', [AgencyLocationController::class, 'show']);
+    Route::post('location-store', [AgencyLocationController::class, 'store']);
+    Route::put('location-update/{id}', [AgencyLocationController::class, 'update']);
+    Route::put('location-bulk-update', [AgencyLocationController::class, 'bulkUpdate']);
+    Route::delete('location-destroy/{id}', [AgencyLocationController::class, 'destroy']);
+
+    // agency checklist management route
+    Route::get('checklist', [AgencyChecklistController::class, 'index']);
+    Route::get('checklist/{id}', [AgencyChecklistController::class, 'show']);
+    Route::post('checklist-store', [AgencyChecklistController::class, 'store']);
+    Route::put('checklist-update/{id}', [AgencyChecklistController::class, 'update']);
+    Route::put('checklist-bulk-update', [AgencyChecklistController::class, 'bulkUpdate']);
+    Route::delete('checklist-destroy/{id}', [AgencyChecklistController::class, 'destroy']);
+
+    // agency tags management route
+    Route::get('tags', [AgencyTagController::class, 'index']);
+    Route::get('tag/{id}', [AgencyTagController::class, 'show']);
+    Route::post('tag-store', [AgencyTagController::class, 'store']);
+    Route::put('tag-update/{id}', [AgencyTagController::class, 'update']);
+    Route::put('tag-bulk-update', [AgencyTagController::class, 'bulkUpdate']);
+    Route::delete('tag-destroy/{id}', [AgencyTagController::class, 'destroy']);
+
+    // agency event type management route
+    Route::get('event-types', [AgencyEventTypeController::class, 'index']);
+    Route::get('event-type/{id}', [AgencyEventTypeController::class, 'show']);
+    Route::post('event-type-store', [AgencyEventTypeController::class, 'store']);
+    Route::put('event-type-update/{id}', [AgencyEventTypeController::class, 'update']);
+    Route::put('event-type-bulk-update', [AgencyEventTypeController::class, 'bulkUpdate']);
+    Route::delete('event-type-destroy/{id}', [AgencyEventTypeController::class, 'destroy']);
+
+    // agency event management route
+    Route::get('events', [AgencyEventController::class, 'index']);
+    Route::post('event-store', [AgencyEventController::class, 'store']);
+    Route::get('event-show/{id}', [AgencyEventController::class, 'show']);
+    Route::put('event-update/{id}', [AgencyEventController::class, 'update']);
+    Route::delete('event-destroy/{id}', [AgencyEventController::class, 'destroy']);
+
+    // agency status template (get-process-flow) management route
+    Route::get('get-process-flow', [StatusTemplateController::class, 'getProcessFlow']);
+    Route::post('status-template-store', [StatusTemplateController::class, 'store']);
+    Route::get('status-template-show/{id}', [StatusTemplateController::class, 'show']);
+    Route::put('status-template-update/{id}', [StatusTemplateController::class, 'update']);
+    Route::delete('status-template-destroy/{id}', [StatusTemplateController::class, 'destroy']);
+    // status template create new status after
+    Route::post('statuses/store-after', [StatusTemplateController::class, 'storeAfter']);
+
+    // document template management route
+    Route::get('document-templates', [DocumentTemplateController::class, 'index']);
+    Route::post('document-templates/store', [DocumentTemplateController::class, 'store']);
+    Route::get('document-templates/show/{id}', [DocumentTemplateController::class, 'show']);
+    Route::post('document-templates/update/{id}', [DocumentTemplateController::class, 'update']);
+    Route::delete('document-templates/delete/{id}', [DocumentTemplateController::class, 'destroy']);
+
+    // Client Status
+    Route::get('/statuses', [StatusController::class, 'index']);
+    Route::post('/status-store', [StatusController::class, 'store']);
+    Route::get('/status-edit/{id}', [StatusController::class, 'edit']);
+    Route::put('/status-update/{id}', [StatusController::class, 'update']);
+    Route::patch('/status-serial-update/{id}', [StatusController::class, 'serial']);
+    Route::delete('/status-delete/{id}', [StatusController::class, 'destroy']);
+    // reasons
+    Route::post('/status-reasons', [StatusController::class, 'storeReasons']);
+
+    // Form
+    Route::post('/forms', [FormController::class, 'store']);
+    Route::post('/form-fields', [FormFieldController::class, 'store']);
+
+    Route::post('/form-fields/reorder', [FormFieldController::class, 'reorder']);
+    Route::get('/forms/{slug}', [FormController::class, 'show']);
+
+    // Client Registration
+    Route::post('/clients', [ClientController::class, 'store']);
+    Route::get('/clients/{id}', [ClientController::class, 'show']);
+});
---
+++ routes/client.php
@@ -0,0 +1,154 @@
+<?php
+
+use App\Http\Controllers\Agency\AgencySettingsController;
+use App\Http\Controllers\Client\ClientCandidateController;
+use App\Http\Controllers\Client\ClientDashboardController;
+use App\Http\Controllers\Client\ClientDocumentController;
+use App\Http\Controllers\Client\ClientInterviewController;
+use App\Http\Controllers\Client\ClientLocationController;
+use App\Http\Controllers\Client\ClientNotificationController;
+use App\Http\Controllers\Client\ClientPaymentController;
+use App\Http\Controllers\Client\ClientProfileController;
+use App\Http\Controllers\Client\JobMessageController as ClientJobMessageController;
+use App\Http\Controllers\Client\LongTermJobApplicationController as ClientLongTermJobApplicationController;
+use App\Http\Controllers\Client\LongTermJobAttendanceController as ClientLongTermJobAttendanceController;
+use App\Http\Controllers\Client\LongTermJobController;
+use App\Http\Controllers\Client\LongTermJobPaymentController as ClientLongTermJobPaymentController;
+use App\Http\Controllers\Client\LongTermJobRefundController as ClientLongTermJobRefundController;
+use App\Http\Controllers\Client\LongTermJobReviewController as ClientLongTermJobReviewController;
+use App\Http\Controllers\Client\ShortTermJobApplicationController as ClientShortTermJobApplicationController;
+use App\Http\Controllers\Client\ShortTermJobController;
+use App\Http\Controllers\Client\ShortTermJobPaymentController as ClientShortTermJobPaymentController;
+use App\Http\Controllers\Client\ShortTermJobRefundController as ClientShortTermJobRefundController;
+use App\Http\Controllers\Client\ShortTermJobReviewController as ClientShortTermJobReviewController;
+use App\Http\Controllers\Messages\ShortTermJobMessageController;
+use Illuminate\Support\Facades\Route;
+
+Route::middleware(['subdomain', 'auth:api', 'role:client'])->prefix('client')->group(function () {
+    // Dashboard
+    Route::get('dashboard', [ClientDashboardController::class, 'index']);
+
+    // Profile management
+    Route::get('profile', [ClientProfileController::class, 'show']);
+    Route::put('profile', [ClientProfileController::class, 'update']);
+    Route::put('profile/password', [ClientProfileController::class, 'updatePassword']);
+    Route::delete('profile', [ClientProfileController::class, 'destroy']);
+
+    // Locations (dropdown source for profile / job address)
+    Route::get('locations', [ClientLocationController::class, 'index']);
+
+    // Notifications
+    Route::get('notifications', [ClientNotificationController::class, 'index']);
+    Route::put('notifications/mark-all-read', [ClientNotificationController::class, 'markAllRead']);
+    Route::put('notifications/{id}/read', [ClientNotificationController::class, 'markRead']);
+    Route::delete('notifications/{id}', [ClientNotificationController::class, 'destroy']);
+
+    // Documents / Agreements
+    Route::get('documents', [ClientDocumentController::class, 'index']);
+    Route::get('documents/{document}', [ClientDocumentController::class, 'show']);
+    Route::post('documents/{document}/sign', [ClientDocumentController::class, 'sign']);
+    Route::get('documents/{document}/download', [ClientDocumentController::class, 'download']);
+
+    // Aggregated payments hub
+    Route::get('payments', [ClientPaymentController::class, 'index']);
+    Route::get('payments/invoices', [ClientPaymentController::class, 'invoices']);
+    Route::get('payments/invoices/{invoice}', [ClientPaymentController::class, 'showInvoice']);
+    Route::post('payments/invoices/{invoice}/pay', [ClientPaymentController::class, 'payInvoice']);
+    Route::get('payments/{paymentId}/download', [ClientPaymentController::class, 'download']);
+
+    // Candidates browsing
+    Route::get('candidates/discover', [ClientCandidateController::class, 'discover']);
+    Route::get('candidates', [ClientCandidateController::class, 'index']);
+    Route::get('candidates/{candidate}', [ClientCandidateController::class, 'show']);
+
+    // Candidate reviews (not tied to a single job)
+    Route::get('candidates/{candidate}/reviews', [ClientCandidateController::class, 'reviews']);
+    Route::post('candidates/{candidate}/reviews', [ClientCandidateController::class, 'storeReview']);
+
+    // Hire / Interview requests from candidate detail page
+    Route::post('candidates/{candidate}/hire-request', [ClientCandidateController::class, 'hireRequest']);
+    Route::post('candidates/{candidate}/interview-request', [ClientCandidateController::class, 'interviewRequest']);
+
+    // Interviews management
+    Route::get('interviews', [ClientInterviewController::class, 'index']);
+    Route::get('interviews/{interview}', [ClientInterviewController::class, 'show']);
+    Route::put('interviews/{interview}/reschedule', [ClientInterviewController::class, 'reschedule']);
+    Route::delete('interviews/{interview}', [ClientInterviewController::class, 'cancel']);
+
+    // Check agency payment settings before showing the job form
+    Route::get('jobs/short-term/payment-check', [AgencySettingsController::class, 'clientShortTermPaymentCheck']);
+
+    // Short-term jobs
+    Route::get('jobs/short-term', [ShortTermJobController::class, 'index']);
+    Route::post('jobs/short-term', [ShortTermJobController::class, 'store']);
+    Route::get('jobs/short-term/{shortTermJob}', [ShortTermJobController::class, 'show']);
+    Route::put('jobs/short-term/{shortTermJob}', [ShortTermJobController::class, 'update']);
+    Route::delete('jobs/short-term/{shortTermJob}', [ShortTermJobController::class, 'destroy']);
+    Route::put('jobs/short-term/{shortTermJob}/cancel', [ShortTermJobController::class, 'cancel']);
+    Route::put('jobs/short-term/{shortTermJob}/resubmit', [ShortTermJobController::class, 'resubmit']);
+    Route::post('jobs/short-term/{shortTermJob}/broadcast', [ShortTermJobController::class, 'broadcastRequest']);
+
+    // Client short-term applicants
+    Route::get('jobs/short-term/{shortTermJob}/applicants', [ClientShortTermJobApplicationController::class, 'index']);
+    Route::get('jobs/short-term/{shortTermJob}/applicants/{applicationId}', [ClientShortTermJobApplicationController::class, 'show']);
+    Route::get('jobs/short-term/{shortTermJob}/applicants/{applicationId}/candidate-reviews', [ClientShortTermJobApplicationController::class, 'candidateReviews']);
+    Route::post('jobs/short-term/{shortTermJob}/applicants/{applicationId}/hire', [ClientShortTermJobApplicationController::class, 'hire']);
+
+    // Client short-term invoice & payments
+    Route::get('jobs/short-term/{shortTermJob}/invoice', [ClientShortTermJobPaymentController::class, 'invoice']);
+    Route::get('jobs/short-term/{shortTermJob}/payments', [ClientShortTermJobPaymentController::class, 'index']);
+    Route::post('jobs/short-term/{shortTermJob}/payments', [ClientShortTermJobPaymentController::class, 'store']);
+    Route::get('jobs/short-term/{shortTermJob}/payments/{paymentId}/download', [ClientShortTermJobPaymentController::class, 'download']);
+
+    // Client short-term reviews
+    Route::get('jobs/short-term/{shortTermJob}/reviews', [ClientShortTermJobReviewController::class, 'index']);
+    Route::post('jobs/short-term/{shortTermJob}/reviews', [ClientShortTermJobReviewController::class, 'store']);
+
+    // Client short-term refund
+    Route::get('jobs/short-term/{shortTermJob}/refund-request', [ClientShortTermJobRefundController::class, 'show']);
+    Route::post('jobs/short-term/{shortTermJob}/refund-request', [ClientShortTermJobRefundController::class, 'store']);
+
+    // Client short-term messages
+    Route::get('jobs/short-term/{shortTermJob}/messages', [ShortTermJobMessageController::class, 'index']);
+    Route::post('jobs/short-term/{shortTermJob}/messages', [ShortTermJobMessageController::class, 'store']);
+    Route::get('jobs/short-term/{shortTermJob}/messages/unread-counts', [ShortTermJobMessageController::class, 'unreadCounts']);
+
+    // Long-term jobs
+    Route::get('jobs/long-term', [LongTermJobController::class, 'index']);
+    Route::post('jobs/long-term', [LongTermJobController::class, 'store']);
+    Route::get('jobs/long-term/{longTermJob}', [LongTermJobController::class, 'show']);
+    Route::put('jobs/long-term/{longTermJob}', [LongTermJobController::class, 'update']);
+    Route::delete('jobs/long-term/{longTermJob}', [LongTermJobController::class, 'destroy']);
+    Route::put('jobs/long-term/{longTermJob}/cancel', [LongTermJobController::class, 'cancel']);
+    Route::put('jobs/long-term/{longTermJob}/resubmit', [LongTermJobController::class, 'resubmit']);
+    Route::post('jobs/long-term/{longTermJob}/broadcast', [LongTermJobController::class, 'broadcastRequest']);
+
+    // Client long-term attendance calendar (read-only)
+    Route::get('jobs/long-term/{longTermJob}/attendance', [ClientLongTermJobAttendanceController::class, 'calendar']);
+
+    // Client long-term applicants (marketplace jobs)
+    Route::get('jobs/long-term/{longTermJob}/applicants', [ClientLongTermJobApplicationController::class, 'index']);
+    Route::get('jobs/long-term/{longTermJob}/applicants/{application}', [ClientLongTermJobApplicationController::class, 'show']);
+    Route::get('jobs/long-term/{longTermJob}/applicants/{application}/candidate-reviews', [ClientLongTermJobApplicationController::class, 'candidateReviews']);
+    Route::post('jobs/long-term/{longTermJob}/applicants/{application}/interview', [ClientLongTermJobApplicationController::class, 'interview']);
+    Route::post('jobs/long-term/{longTermJob}/applicants/{application}/hire', [ClientLongTermJobApplicationController::class, 'hire']);
+
+    // Client long-term invoice & payments
+    Route::get('jobs/long-term/{longTermJob}/invoice', [ClientLongTermJobPaymentController::class, 'invoice']);
+    Route::get('jobs/long-term/{longTermJob}/payments', [ClientLongTermJobPaymentController::class, 'index']);
+    Route::post('jobs/long-term/{longTermJob}/payments', [ClientLongTermJobPaymentController::class, 'store']);
+    Route::get('jobs/long-term/{longTermJob}/payments/{payment}/download', [ClientLongTermJobPaymentController::class, 'download']);
+
+    // Client long-term reviews
+    Route::get('jobs/long-term/{longTermJob}/reviews', [ClientLongTermJobReviewController::class, 'index']);
+    Route::post('jobs/long-term/{longTermJob}/reviews', [ClientLongTermJobReviewController::class, 'store']);
+
+    // Client long-term refund
+    Route::get('jobs/long-term/{longTermJob}/refund-request', [ClientLongTermJobRefundController::class, 'show']);
+    Route::post('jobs/long-term/{longTermJob}/refund-request', [ClientLongTermJobRefundController::class, 'store']);
+
+    // Client long-term job messages (client thread only)
+    Route::get('jobs/long-term/{longTermJob}/messages', [ClientJobMessageController::class, 'index']);
+    Route::post('jobs/long-term/{longTermJob}/messages', [ClientJobMessageController::class, 'store']);
+    Route::get('jobs/long-term/{longTermJob}/messages/unread-counts', [ClientJobMessageController::class, 'unreadCounts']);
+});
