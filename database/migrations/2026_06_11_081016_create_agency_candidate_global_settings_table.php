00dbabc80fca253c133ba9e9ba09c8594f779397
Niaz Ahmed Nayeem <niazahmed.net@gmail.com>
2026-06-11 17:46:22 +0600

    create candidate global setting model and schema file

---
+++ database/migrations/2026_06_11_081016_create_agency_candidate_global_settings_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('agency_candidate_global_settings', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('agency_id')->unique();
+            $table->json('settings')->nullable();
+            $table->timestamps();
+
+            $table->foreign('agency_id')->references('id')->on('agencies')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('agency_candidate_global_settings');
+    }
+};
