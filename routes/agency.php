8be5faa44f20fc94ecedb879217170339646826c
Niaz Ahmed Nayeem <niazahmed.net@gmail.com>
2026-06-11 11:51:05 +0600

    added agency global sob settings routes

--- routes/agency.php
+++ routes/agency.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Agency\AgencyChecklistController;
 use App\Http\Controllers\Agency\AgencyController;
 use App\Http\Controllers\Agency\AgencyEventController;
 use App\Http\Controllers\Agency\AgencyEventTypeController;
+use App\Http\Controllers\Agency\AgencyGlobalJobSettingsController;
 use App\Http\Controllers\Agency\AgencyLocationController;
 use App\Http\Controllers\Agency\AgencyNoteController;
 use App\Http\Controllers\Agency\AgencySettingsController;
@@ -141,6 +142,11 @@ Route::middleware(['subdomain', 'auth:api', 'role:agency_admin'])->prefix('agenc
     Route::get('settings/communication', [SettingsController::class, 'getCommunicationSettings']);
     Route::post('settings/communication', [SettingsController::class, 'updateCommunicationSettings']);
 
+    // agency global job settings management route for global settings
+    Route::get('/get-global-job-settings', [AgencyGlobalJobSettingsController::class, 'getGlobalJobSettings']);
+    Route::post('/update-global-job-settings', [AgencyGlobalJobSettingsController::class, 'updateGlobalJobSettings']);
+
+    // agency global info management route
     Route::get('/agency-info', [AgencyController::class, 'info']);
     Route::post('info-update/{id}', [AgencyController::class, 'infoUpdate']);
 });
